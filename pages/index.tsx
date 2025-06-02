4ed1f7555c1d34c409540951449f4e84a9a6b5c5
George Colon <geocolon@gmail.com>
2025-06-02 01:12:52 -0700

    updated index.tsx and Header.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,11 @@
-// import Image from "next/image";
-// import { Geist, Geist_Mono } from "next/font/google";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
    <>
-    <body>
-      <h1 className="text-3xl font-bold underline text-sky-500">
-        Hello world!
-      </h1>
-    </body>
+    <Header showBackArrow label="Home" />
+    
+      
    </>
   );
 }
